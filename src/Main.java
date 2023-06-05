112921eea79fa766da5338eed804c370e0c23074
unknown <gabrielgb4230@gmail.com>
2023-06-05 18:04:09 -0300

    testes

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         Pattern p = Pattern.compile("^[A-Za-z0-9]{1,}[@]{1}[a-z]{1,}.com$");
-        Matcher m = p.matcher("gabr22iel@outlook.com");
+        Matcher m = p.matcher("2342345345345@gmail.com");
         boolean teste = m.find();
 
         System.out.println(teste);
 
         // gabriel@gmail.com | TRUE
         // gabr22iel@outlook.com | TRUE
+        //  gabr@gmail.com | FALSE
+        // +gab@gmail.com | FALSE
+        // gabriel@@@@gmail.com | FALSE
+
     }
 }
